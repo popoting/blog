50f5bf4010dc916e593e6f05ca4e06d63278ef06
你的名字 <email@xxx.xxx>
2017-03-31 18:41:50 +0800

    Added comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,51 @@
 class PostsController < ApplicationController
-  def index
-    @posts =Post.all.order('created_at DESC')
-  end
+  
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
 
-  def new
-    @post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
+	def show
+		@post = Post.find(params[:id])
+	end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+	def create
+		@post = Post.new(post_params)
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
 
-  def update
-    @post = Post.find(parans[:id])
-    if @post.update(params[:post].permit(:title, :body))
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
+	def edit
+		@post = Post.find(params[:id])
+	end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path
+	def update
+		@post = Post.find(params[:id])
 
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-  private
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
 
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
